3df70aee59b5260c3c50d1a48ed362737b16cd05
biswajit91237 <39646826+biswajit9123@users.noreply.github.com>
2018-12-03 11:26:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,6 +73,14 @@ async def pat(ctx):
 async def kill(ctx):
   await bot.say("Your wish shall be granted!!!! That Your daddy even wont know😈. I ll see you while you sleep FOREVER!!!!"+" "+ctx.message.author.name)
 
+@bot.command(pass_context=True)
+async def badday(ctx):
+  await bot.say("Hehe... cause Am having a good day😂"+" "+ctx.message.author.name)
+  
+@bot.command(pass_context=True)
+async def help(ctx):
+  await bot.say("▪We Will Contact  @Nøøb Gamer#3762 Sum More Information For This Bot.▪ "+" "+ctx.message.author.name) 
 
+  
 #YOU CAN USE os.environ TO HIDE YOUR BOT TOKEN: SAVE YOUR BOT TOKEN AS THE NAME YOU GAVE IN os.environ['name'] 
 bot.run(os.environ['BOT_TOKEN'])
