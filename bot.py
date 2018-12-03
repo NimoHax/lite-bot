8820c2b9ffb4a04281d2bdc968a54bcf211ff2e7
biswajit91237 <39646826+biswajit9123@users.noreply.github.com>
2018-12-03 08:11:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,30 @@
 import discord
-from discord.ext import commands
-import asyncio
-import os
 
+client = discord.Client()
 
-#GIVE YOUR BOT A PREFIX; mine is a.
-bot = commands.Bot(command_prefix="a.")
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
 
+    if message.content.startswith('Hello'):
+        msg = 'Hello Friend {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
 
+    if message.content.startswith('Gm'):
+        msg = 'Good Morning {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
 
+    if message.content.startswith('Hv'):
+        msg = 'Have a nice day {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
 
-#PRINT THE DISCORD BOT'S NAME WHEN IT'S READY
-@bot.event
+@client.event
 async def on_ready():
-  print(bot.user.name)
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
-  
-#A SIMPLE TEST COMMAND
-@bot.command(pass_context=True)
-async def hi(ctx):
-  await bot.say("Hello there"+" "+ctx.message.author.name)
-  
-  
- 
-#YOU CAN USE os.environ TO HIDE YOUR BOT TOKEN: SAVE YOUR BOT TOKEN AS THE NAME YOU GAVE IN os.environ['name'] 
-bot.run(os.environ['BOT_TOKEN'])
+client.run('BOT_TOKEN')
