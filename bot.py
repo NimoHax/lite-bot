a5d539c999e1c2300082725e86be37779212a75f
biswajit91237 <39646826+biswajit9123@users.noreply.github.com>
2018-12-03 11:32:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,10 +74,6 @@ async def kill(ctx):
   await bot.say("Your wish shall be granted!!!! That Your daddy even wont know😈. I ll see you while you sleep FOREVER!!!!"+" "+ctx.message.author.name)
 
 @bot.command(pass_context=True)
-async def badday(ctx):
-  await bot.say("Hehe... cause Am having a good day😂"+" "+ctx.message.author.name)
-  
-@bot.command(pass_context=True)
 async def help(ctx):
   await bot.say("▪We Will Contact  Nøøb Gamer Sum More Information For This Bot.▪ "+" "+ctx.message.author.name) 
 
